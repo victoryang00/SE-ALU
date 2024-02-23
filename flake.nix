5f80278222e6adf30a4301ec7c800e0f5da0de91
Amin Karbas <mkarbasf@ucsc.edu>
2024-02-23 13:59:52 -0800

    Add nix dev env flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default =
+          pkgs.mkShellNoCC {
+            name = "rules_nixpkgs_shell";
+            packages = with pkgs; [ sbt scala ];
+          };
+      });
+}
