067cb1d83dfb1321b045d561f8533b5c6dd446fc
Amin Karbas <mkarbasf@ucsc.edu>
2024-03-11 01:51:16 -0700

    Add verilator

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-verilator.url = "github:NixOS/nixpkgs/c459a3638a5bbcd92f14d0b73338ceb1609a0a1d";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, nixpkgs-verilator, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pkgs-verilator = import nixpkgs-verilator { inherit system; };
       in {
         devShells.default =
-          pkgs.mkShellNoCC {
+          pkgs.mkShell {
             name = "rules_nixpkgs_shell";
-            packages = with pkgs; [ sbt scala ];
+            packages = with pkgs; [ sbt scala pkgs-verilator.verilator ];
           };
       });
 }
